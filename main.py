19283a25debad7f780fca57a35a8dd9c74966dd9
Prathima Kadari <kadariprathima4@gmail.com>
2022-01-06 17:01:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ st.sidebar.markdown('<h1 style="margin-left:8%; color:	#FA8072 ">Kutch Water Qua
 
 add_selectbox = st.sidebar.radio(
     "",
-    ("Home", "About", "Features", "Safest Path", "Maps", "Visualizations", "Conclusion", "Team")
+    ("Home", "About", "Features", "Select AOI Data Parameters", "Our Result", "Visualizations", "Conclusion", "Team")
 )
 
 if add_selectbox == 'Home':
     
-    LOGO_IMAGE = "omdena_kutch_logo.jpg"
+    LOGO_IMAGE = "omdena_india_logo.png"
     
     st.markdown(
           """
@@ -60,7 +60,7 @@ if add_selectbox == 'Home':
     st.markdown(
           f"""
           <div class="container">
-               <img class="logo-img" src="data:image/jpg;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+               <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
           </div>
           """,
           unsafe_allow_html=True
@@ -68,46 +68,42 @@ if add_selectbox == 'Home':
     
     st.subheader('PROBLEM STATEMENT')
     
-    st.markdown('Natural Disasters are problems in Japan, with risk of earthquakes, floods and tsunamis. Japan has well-developed \
-        disaster response systems, but densely populated cities and narrow roads make managing the response difficult. By giving \
-            individuals information about the safest ways from their homes and places of work, it will increase their awareness of \
-                the surrounding area and improve their preparedness.', unsafe_allow_html=True)
+    st.markdown('', unsafe_allow_html=True)
 
 elif add_selectbox == 'About':
     
     st.subheader('ABOUT THE PROJECT')
 
     st.markdown('<h4>Project Goals</h4>', unsafe_allow_html=True)
-    st.markdown('• collect satellite images and identify road characteristics', unsafe_allow_html=True) 
-    st.markdown('• build a model for scoring the roads in terms of their suitability for use in emergency', unsafe_allow_html=True) 
-    st.markdown('• build a pathfinding model from A to B, combining it with road characteristics', unsafe_allow_html=True) 
-    st.markdown('• suggest safest path from A to B', unsafe_allow_html=True) 
-    st.markdown('• publish interactive dashboards to display road characteristics and safest paths', unsafe_allow_html=True) 
-    st.markdown('• arrange demonstration and publicise to local audiences', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
+    st.markdown('• ', unsafe_allow_html=True) 
     
     st.markdown('<h4>Location Choosen</h4>', unsafe_allow_html=True)
-    st.markdown('We had choosen "Nakagawa-Ku as our region of interest, which comes under Aichi prefecture of Nagoya City. It comes under Chubu region and \
-        is the 4th densely populated city in Japan with high risk prone to disasters.',
+    st.markdown('',
                 unsafe_allow_html=True)
     
     st.markdown('<h4>Developments Made</h4>', unsafe_allow_html=True)
-    st.markdown('We had designed a model collecting data about the local roads from satellite images, classify them and indicate the safest \
-        route to be taken from point A to point B and an interactive dashboard to display the safest route in a map.',
+    st.markdown(' \
+        ',
                 unsafe_allow_html=True)
-    st.markdown('By making individuals aware, it will improve their preparedness and it can be used within families to prepare disaster \
-        response plans, depending on their circumstances. To be used by individuals, families and groups, and foreign residents who may \
-            not understand local information. Further development will be covering more geographical areas and publicising on a local level.'
+    st.markdown(' \
+         \
+            '
                 , unsafe_allow_html=True)
     
 elif add_selectbox == 'Features':
 
     st.subheader('PROJECT ENDORSEMENTS')
 
-    st.markdown('• Safest route path to take at occurences of japan disasters', unsafe_allow_html=True)
-    st.markdown('• Locates shelters in Nakagawa Ward - Earthquakes, Tsunamis and Floods', unsafe_allow_html=True)
-    st.markdown('• Visualizations to Check and Differentiate Parameters across the Nakagawa Ward', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
+    st.markdown('• ', unsafe_allow_html=True)
     
-elif add_selectbox == 'Safest Path':
+elif add_selectbox == 'Select AOI Data Parameters':
     st.subheader('Find Safest Path')
 
     sentence = st.text_input('Input your current location:')
@@ -132,8 +128,8 @@ elif add_selectbox == 'Safest Path':
 
    # folium_static(route_map, width=900)
 
-elif add_selectbox == 'Maps':
-    st.subheader('Maps')
+elif add_selectbox == 'Result':
+    st.subheader('Our Result')
 
     col1, col2 = st.columns(2)
 
@@ -190,13 +186,13 @@ elif add_selectbox == 'Visualizations':
     
     
     st.subheader('PROJECT VISUALIZATIONS')
-    st.markdown('<h4>Japan Earthquake Zoning Areas</h4>', unsafe_allow_html=True)
+    st.markdown('<h4></h4>', unsafe_allow_html=True)
     st.image("Japan_Earthquakes_Zoning.png", width=500)
-    st.markdown('<h4>Nakagawa Shelter Maps</h4>', unsafe_allow_html=True)
+    st.markdown('<h4></h4>', unsafe_allow_html=True)
     st.image("Nakagawa_Shelter_Maps.png", width=500)
-    st.markdown('<h4>Nakagawa Building Density Score</h4>', unsafe_allow_html=True)
+    st.markdown('<h4></h4>', unsafe_allow_html=True)
     st.image("Nakagawa_Building_Density_Score.png", width=500)
-    st.markdown('<h4>Nakagawa Distance Risk Score</h4>', unsafe_allow_html=True)
+    st.markdown('<h4></h4>', unsafe_allow_html=True)
     st.image("Nakagawa_Distance_Risk_Score.png", width=500)
    
     
