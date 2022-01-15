9698820142d5c0064102e1bca8207dc84028c564
Tanisha Banik <26tanishabanik@gmail.com>
2022-01-15 23:07:42 +0530

    c

--- main.py
+++ main.py
@@ -270,11 +270,13 @@ elif add_selectbox == 'Select AOI Data Parameters':
         
     if st.button('Submit'):
         
-
-        df2 = get_data(long, lat, slider1, slider2) 
+        latlon, starting = get_data(long, lat, slider1, slider2)
+        st.write(latlon)
+        st.write(starting)
+        # df2 = get_data(long, lat, slider1, slider2) 
         # st.write(mess)
-        df_all, test = send_df(df2)
-        st.write(df_all)
+        # df_all, test = send_df(df2)
+        # st.write(df_all)
 
         if prm_type == 'Dissolved Oxygen':
             plot_do(df_all)
